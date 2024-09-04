7a38783f5791e75fb64aa8fdf28fd386b408d144
dm-hussain <message2hussain@gmail.com>
2024-09-04 10:33:23 +0530

    fixed some bugs in input section when input becomes empty after chechkmark the chechkmark soulld be removed

--- script.js
+++ script.js
@@ -1,115 +1,104 @@
 let checkBoxList = document.querySelectorAll('.custom-check-box');
 let inputList = document.querySelectorAll('.goal-title');
 const warningMsg = document.querySelector('#warning-msg');
-let progressValue= document.querySelector('.progress-value')
-
-
+let progressValue = document.querySelector('.progress-value');
 
 let userInput;
 let userInputValue;
-const emptyArr=[];
-let initialProgressValue=0;
-progressValue.style.width= initialProgressValue +'%';
-updateProgressBarText()
+const emptyArr = [];
+let initialProgressValue = 0;
+progressValue.style.width = initialProgressValue + '%';
+updateProgressBarText();
 
 checkBoxList.forEach((checkBox, i) => {
 
-  
 
- 
-   
+
+
   checkBox.addEventListener('click', () => {
-   
-  
- 
-  emptyArr.length=0;
-   
+    emptyArr.length = 0;
+
     inputList.forEach((el) => {
       if (el.value === '') {
-        emptyArr.push('true') ;
-      } 
       
-      else{
-        emptyArr.push('false') ;
+        emptyArr.push('true');
+      } else {
+        emptyArr.push('false');
       }
-
-      
     });
 
     userInputValue = inputList[i].value;
 
-    if (userInputValue !== '' && (emptyArr[0]==='false' && emptyArr[1]=== 'false' && emptyArr[2]==='false') ) {
+    if (
+      userInputValue !== '' &&
+      emptyArr[0] === 'false' &&
+      emptyArr[1] === 'false' &&
+      emptyArr[2] === 'false'
+    ) {
       checkBox.parentElement.classList.toggle('completed');
       warningMsg.classList.add('hide');
 
-      if(checkBox.parentElement.classList.contains('completed')) {
-        
-        initialProgressValue+= 33.33;
-        progressValue.style.width=  initialProgressValue + '%' ;
-        progressValue.style.visibility=  'visible';
-        updateProgressBarText()
-
+      if (checkBox.parentElement.classList.contains('completed')) {
+        initialProgressValue += 33.33;
+        progressValue.style.width = initialProgressValue + '%';
+        progressValue.style.visibility = 'visible';
+        updateProgressBarText();
+      } else {
+        initialProgressValue -= 33.33;
+        progressValue.style.width = initialProgressValue + '%';
+        updateProgressBarText();
       }
-      
-         else{
-        initialProgressValue-= 33.33;
-        progressValue.style.width=  initialProgressValue + '%' ;
-        updateProgressBarText()
-      }
-    } 
-    
-    
-    else {
+    } else {
       warningMsg.classList.remove('hide');
-       inputList.forEach((input) => {
+      inputList.forEach((input) => {
+      
 
-        input.addEventListener('focus', () =>{
-
+        input.addEventListener('focus', () => {
           warningMsg.classList.add('hide');
-
-        })
-       
-
-
-       })
-     
-       
+        });
+      });
     }
+
+
+
+    inputList.forEach((input)=> {
+
+      input.addEventListener('change', () => {
+       
+        
+        if(input.value==='') {
+          debugger
+          initialProgressValue = 0
+          updateProgressBarText()
+
+      checkBox.parentElement.classList.remove('completed');
+      
+       
+        }
+      })
+
+    })
+
   });
 });
 
+function updateProgressBarText() {
+  let progressValue = document.querySelector('.progress-value');
 
- function updateProgressBarText() {
-
-  let progressValue= document.querySelector('.progress-value')
-
-  if(initialProgressValue===0) {
-
-    progressValue.style.visibility=  'hidden';
-    progressValue.innerText= '' ;
-      
+  if (initialProgressValue === 0) {
+    progressValue.style.visibility = 'hidden';
+    progressValue.innerText = '';
+  } else if (initialProgressValue === 33.33) {
+    progressValue.innerText = '1/3 Completed';
+  } else if (initialProgressValue === 66.66) {
+    progressValue.innerText = '2/3 Completed';
   }
 
-  else if(initialProgressValue===33.33) {
-
-    
-    progressValue.innerText= '1/3 Completed' ;
-      
+  if (initialProgressValue === 99.99) {
+    progressValue.innerText = '3/3 Completed';
   }
+}
 
-  else if(initialProgressValue===66.66) {
-
-    
-    progressValue.innerText= '2/3 Completed' ;
-      
-  }
+ 
 
 
-  if(initialProgressValue===99.99) {
-
-    
-    progressValue.innerText= '3/3 Completed' ;
-      
-  }
-
- }
